7205d52a0b7694d33b343a4091e5d4e9c3d1956d
sanch <sanch13sanch@gmail.com>
2024-12-04 10:57:21 +0300

    added loging to api views

--- rooms/views.py
+++ rooms/views.py
@@ -1,11 +1,17 @@
+import datetime
+
 from django.shortcuts import render
+
+from logs.logging_config import logger
 
 
 def first_room(request):
+    logger.info(f"Перезагрука первой комнаты {datetime.datetime.now().strftime('%d %B %Y %H:%M:%S')}")
     return render(request=request,
                   template_name="rooms/first.html")
 
 
 def third_room(request):
+    logger.info(f"Перезагрука третьей комнаты {datetime.datetime.now().strftime('%d %B %Y %H:%M:%S')}")
     return render(request=request,
                   template_name="rooms/third.html")
